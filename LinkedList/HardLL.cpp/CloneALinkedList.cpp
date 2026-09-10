2db1cd6152a9f0c86c146522b9fdf04272d2fc1c
Ashu <ayushprajapati272007@gmail.com>
2026-09-10 22:38:04 +0530

    Clone a Linked List

--- LinkedList/HardLL.cpp/CloneALinkedList.cpp
+++ LinkedList/HardLL.cpp/CloneALinkedList.cpp
@@ -14,3 +14,24 @@ public:
     }
 };
 
+Node* copyRandomList(Node* head) {
+    Node* temp = head;
+    unordered_map<Node*, Node*>mpp;
+
+    while(temp != NULL){
+        Node* newNode = new Node(temp->val);
+        mpp[temp] = newNode;
+        temp = temp->next;
+    }
+
+    temp = head;
+    while(temp != NULL){
+        Node* copyNode = mpp[temp];
+        copyNode->next = mpp[temp->next];
+        copyNode->random = mpp[temp->random];
+        temp = temp->next;
+    }
+
+    return mpp[head];
+}
+
